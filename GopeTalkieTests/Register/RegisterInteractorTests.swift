f13fe06fe50ed6437de3a53cb31d1de336d7acb1
Carlos Mantilla <carlosmcot@gmail.com>
2025-10-05 18:54:51 -0500

    Tests: Add integration tests to the Interactor layer of the Register module

---
+++ GopeTalkieTests/Register/RegisterInteractorTests.swift
@@ -0,0 +1,83 @@
+//
+//  RegisterInteractorTests.swift
+//  GopeTalkieTests
+//
+//  Created by Gopenux on 23/07/25.
+//
+
+import XCTest
+@testable import GopeTalkie
+
+final class RegisterInteractorTests: XCTestCase {
+    var interactor: RegisterInteractor!
+    var mockPresenter: MockRegisterInteractorOutput!
+    var mockRepository: MockRegisterRepository!
+    
+    override func setUp() {
+        super.setUp()
+        
+        mockPresenter = MockRegisterInteractorOutput()
+        mockRepository = MockRegisterRepository()
+        interactor = RegisterInteractor(repository: mockRepository)
+        
+        interactor.presenter = mockPresenter
+    }
+    
+    func testRegisterCallsRepositoryWithRequest() {
+        let request = RegisterRequest(
+            firstName: "Ana",
+            lastName: "Díaz",
+            email: "ana@example.com",
+            password: "123456",
+            confirmPassword: "123456"
+        )
+        interactor.register(user: request)
+        
+        XCTAssertEqual(mockRepository.capturedRequest?.email, "ana@example.com")
+    }
+    
+    func testRepositorySuccessShouldCallPresenterSuccess() {
+        let expectation = self.expectation(description: "Success callback")
+        
+        mockRepository.resultToReturn = .success(RegisterResponse(message: "Cuenta creada"))
+        mockPresenter.onSuccess = {
+            expectation.fulfill()
+        }
+
+        let request = RegisterRequest(
+            firstName: "A",
+            lastName: "B",
+            email: "a@b.com",
+            password: "123",
+            confirmPassword: "123"
+        )
+        interactor.register(user: request)
+
+        waitForExpectations(timeout: 1.0)
+        
+        XCTAssertEqual(mockPresenter.successMessage, "Cuenta creada")
+    }
+    
+    func testRepositoryFailureShouldCallPresenterFailure() {
+        let expectation = self.expectation(description: "Failure callback")
+        
+        let expectedError = NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "Falló el registro"])
+        mockRepository.resultToReturn = .failure(expectedError)
+        mockPresenter.onFailure = {
+            expectation.fulfill()
+        }
+        
+        let request = RegisterRequest(
+            firstName: "A",
+            lastName: "B",
+            email: "a@b.com",
+            password: "123",
+            confirmPassword: "123"
+        )
+        interactor.register(user: request)
+
+        waitForExpectations(timeout: 1.0)
+        
+        XCTAssertEqual(mockPresenter.receivedError?.localizedDescription, "Falló el registro")
+    }
+}
