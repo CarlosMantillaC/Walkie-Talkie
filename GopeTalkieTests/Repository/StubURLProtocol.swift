f233320574468f2147bbe611759a70eced59ea4a
Carlos Mantilla <carlosmcot@gmail.com>
2025-10-05 18:16:47 -0500

    Tests: Add StubURLProtocol to test project repositories

---
+++ GopeTalkieTests/Repository/StubURLProtocol.swift
@@ -0,0 +1,29 @@
+//
+//  MockURLProtocol.swift
+//  GopeTalkieTests
+//
+//  Created by Gopenux on 23/07/25.
+//
+
+import Foundation
+
+class StubURLProtocol: URLProtocol {
+    static var stubResponseData: Data?
+    static var stubError: Error?
+
+    override class func canInit(with request: URLRequest) -> Bool { true }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest { request }
+
+    override func startLoading() {
+        if let error = StubURLProtocol.stubError {
+            self.client?.urlProtocol(self, didFailWithError: error)
+        } else if let data = StubURLProtocol.stubResponseData {
+            self.client?.urlProtocol(self, didLoad: data)
+        }
+
+        self.client?.urlProtocolDidFinishLoading(self)
+    }
+
+    override func stopLoading() {}
+}
